4caa168056f751d79bfe894df872e4200a3cdac6
Vital Fadeev <vital.fadeev@gmail.com>
2025-07-14 11:59:02 +0700

    upd

--- source/app.d
+++ source/app.d
@@ -4,23 +4,23 @@ import appinput  : Events;
 
 void
 main () {
+    struct
+    AppEvent {
+        Type type;
+
+        enum 
+        Type : ushort {
+            _                   = 0,     
+            APP                 = 2^^14,  // 16384
+            START               = APP + 1,
+            DRAW                = APP + 2,
+        }
+    }
+
     auto events = Events!AppEvent ();
     // APP_START
     events ~= AppEvent (AppEvent.Type.START);
 
 	foreach (ref event; events)
 		writeln (event);
-}
-
-struct
-AppEvent {
-    Type type;
-
-    enum 
-    Type : ushort {
-        _                   = 0,     
-        APP                 = 2^^14,  // 16384
-        START               = APP + 1,
-        DRAW                = APP + 2,
-    }
 }
